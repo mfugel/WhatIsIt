36d41a95097eb9d256fc7e52ae3df783f9ace9d9
mfugel <markfugel@tnwinc.com>
2014-03-18 08:39:12 -0400

    Initial commit of WhatIsIt

---
+++ WebApplication1/WhatIsItMainPage.aspx
@@ -0,0 +1,89 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WhatIsItMainPage.aspx.cs" Inherits="WhatIsIt.WhatIsItMainPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <%--<title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 23px;
+            font: 14px 'Segoe UI', tahoma, arial, helvetica, sans-serif;
+            margin: 20px;
+            padding: 0;  
+            align-content:center;     
+        }
+
+        .auto-style2 {
+            height: 23px;
+            font: 14px 'Segoe UI', tahoma, arial, helvetica, sans-serif;
+            margin: 20px;
+            padding: 0;
+            align-content: center;
+            width: 229px;
+        }
+
+    </style>--%>
+<link rel="stylesheet" type="text/css" href="Content/Site.css" media="screen" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <table class="auto-style1;" style="width:100%"; >
+            <tr>
+                <td class="auto-style1"></td>
+                <td class="auto-style2"><asp:Image ID="Logo" runat="server"  ImageUrl="~/Images/WhatIsIt.jpg" ToolTip="WhatIsIt?"  /></td>
+                <td class="auto-style1"></td>            <
+                <td style="text-align:center; font-family: 'Comic Sans MS'; font-size: xx-large; top: auto; bottom: auto; right: auto; left: auto; width: auto; height: auto; ">
+                    What Is It ?</td>
+                <tdWhat Is It ?</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                
+                &nbsp;
+                
+            </tr>
+        </table>
+    
+    </div>
+        <hr />
+        <br />
+        <asp:Button ID="btnShowHideNewImage" runat="server" OnClick="btnShowHideNewImage_Click" Text="Show/Hide Load a New Image" Width="232px" />
+        <br />
+        <br />
+        <asp:Panel ID="UploadPanel" runat="server">
+            Browse to a image file:
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            &nbsp; Name the image (or use the file name):&nbsp;
+            <asp:TextBox ID="txtImageName" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;
+            <asp:Button ID="btnUploadImage" runat="server" OnClick="UploadButton_Click" Text="Upload A New Image" />
+            &nbsp;<asp:Button ID="CancelUpload" runat="server" OnClick="CancelUpload_Click" Text="Cancel" />
+        </asp:Panel>
+        <br />
+        <table border="1" class="auto-style1;" style="width:100%";> <tr>
+            <td style="width:50%";>
+            <asp:Panel ID="Panel1" runat="server"  BorderColor="Blue" BorderStyle="Dotted" HorizontalAlign="Center" ScrollBars="Auto">
+            </asp:Panel>
+            </td>
+             <td style="width:50%";>
+            <asp:Panel ID="Panel2" runat="server" HorizontalAlign="Center">
+                <asp:Label ID="FocusedImageDesc" runat="server" Font-Bold="True" Font-Size="XX-Large" ForeColor="Blue" Text="&lt;===== Click an Image To See What It Is !"></asp:Label>
+                <br />
+                <asp:Image ID="FocusedImage" runat="server" ImageAlign="Middle" />
+                <br />
+                <br />
+                <asp:Label ID="FocusedSound" runat="server"/>
+            </asp:Panel>
+            </td></tr> <tr>
+            <td style="width:50%";>
+                <p><a href="http://www.forvo.com/" title="Pronunciations by Forvo"><img src="http://api.forvo.com/byforvoblue.gif" width="120" height="40" alt="Pronunciations by Forvo" style="border:0" /></a></p></td>
+             <td style="width:50%";>
+                 &nbsp;</td></tr></table>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WhatIsItMainPage.aspx.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Ajax.Utilities;
+using RestSharp;
+
+
+namespace WhatIsIt
+{
+
+    #region // |===== WordSoundService ==========================================================================================|
+
+    public class WordSoundService
+    {
+        public class Attributes
+        {
+            public int Total { get; set; }
+        }
+
+        public class Item
+        {
+            public int Id { get; set; }
+            public string Word { get; set; }
+            public string Addtime { get; set; }
+            public int Hits { get; set; }
+            public string Username { get; set; }
+            public string Sex { get; set; }
+            public string Country { get; set; }
+            public string Code { get; set; }
+            public string Langname { get; set; }
+            public string Pathmp3 { get; set; }
+            public string Pathogg { get; set; }
+            public object Rate { get; set; }
+            public object NumVotes { get; set; }
+            public int NumPositiveVotes { get; set; }
+        }
+
+        public class SoundResult
+        {
+            public Attributes Attributes { get; set; }
+            public List<Item> Items { get; set; }
+        }
+
+        public class SoundRequest
+        {
+            public string SiteUrl { get; set;}
+            public string ApiKey { get; set; }
+            public string Format { get; set; }
+            public string Action { get; set; }
+            
+            public string Language { get; set; }
+            public string Sex { get; set; }
+            public string Order { get; set; }
+            public string Limit { get; set; }
+            public string Type { get; set; }
+        }
+        
+
+        //Constants
+        public const string ApiKey = "114b9ec1f5cb114497cb5b00514bbdc6";
+
+        private const string BaseUrl =
+           "http://apifree.forvo.com" +
+           "/key/" + 
+           ApiKey +
+           "/format/json/action/word-pronunciations/language/en/sex/f/order/rate-desc/limit/1/word/";
+//         "/format/json/action/word-pronunciations/sex/f/order/rate-desc/limit/1/word/";
+        public const string AudioFileType = ".mp3";
+        public const string StorageFolderForSounds = @"UploadedSounds\";
+
+        public static string WordToSound = String.Empty;
+        public static string UrlToGetSoundFile = String.Empty;
+
+        public static string GetSoundFileToPlay(string word)
+        {
+            if (word == null) throw new ArgumentNullException("word");
+            var fullPathSoundStorageLocationForApp = System.AppDomain.CurrentDomain.BaseDirectory +
+                                                     StorageFolderForSounds;
+            
+            // Check if sound file is already there, if so...just return it
+            if (File.Exists(fullPathSoundStorageLocationForApp + word + AudioFileType))
+                return StorageFolderForSounds + word + AudioFileType;
+
+            // Sound file for this word is not there. We need to get it
+            WordToSound = word;
+
+            // Construct the request parameter settings
+            var soundRequest = new SoundRequest { 
+                                                  SiteUrl = "http://apifree.forvo.com",
+                                                  ApiKey = "114b9ec1f5cb114497cb5b00514bbdc6",
+                                                  Format = "json",
+                                                  Action = "word-pronunciations",
+                                                  Language = "en", 
+                                                  Sex = "f", 
+                                                  Limit = "1", 
+                                                  Order = "rate-desc", 
+                                                  Type = "word"
+                                                };
+            
+            UrlToGetSoundFile = BaseUrl + WordToSound;
+            var soundFileUri = GetSoundFromForvo();
+            
+            if (soundFileUri == String.Empty) return String.Empty;
+            
+            var soundFileName = DownloadSoundFile(soundFileUri);
+            
+            if (soundFileName == String.Empty) return String.Empty;
+            
+            return StorageFolderForSounds + soundFileName;
+        }
+
+        public static string GetSoundFromForvo()
+        {
+            var request = new RestRequest();
+            var soundresult = Execute<SoundResult>(request);
+            if (soundresult != null)
+            {
+                if (soundresult.Items.Count > 0)
+                {
+                    var pathmp3 = soundresult.Items[0].Pathmp3;
+                    if (!pathmp3.IsNullOrWhiteSpace())
+                    {
+                        return pathmp3;
+                    }
+                }
+            }
+            return String.Empty;
+        }
+
+        public static T Execute<T>(RestRequest request) where T : new()
+        {
+            //**** TBD: turn this into client.add parameters instead of hardcoded BaseUrl****
+            //IRestClient client = new RestClient("http://apifree.com/");
+            //client.AddDefaultHeader("key", ApiKey);
+            //client.AddDefaultUrlSegment("key", ApiKey);
+            //client.AddDefaultUrlSegment("language", "en");
+            //client.AddDefaultUrlSegment("format", "json");
+            //client.AddDefaultUrlSegment("action", "word-pronunciations");
+            //client.AddDefaultUrlSegment("limit", "1");
+            //client.AddDefaultUrlSegment("type", "word");
+
+            var client = new RestClient {BaseUrl = UrlToGetSoundFile};
+            var response = client.Execute<T>(request);
+            if (response.ErrorException != null)
+            {
+                const string message = "Error retrieving response from Forvo api. Check inner details for more info.";
+                var forvoException = new ApplicationException(message, response.ErrorException);
+                throw forvoException;
+            }
+            return response.Data;
+        }
+
+        public static string DownloadSoundFile(string soundFileUri)
+        {
+            var u = new Uri(soundFileUri);
+            var fileName = WordToSound + AudioFileType;
+            var pathWithSoundFileName = System.AppDomain.CurrentDomain.BaseDirectory + "UploadedSounds\\" + fileName;
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(soundFileUri, pathWithSoundFileName);
+            }
+            return fileName;
+        }
+
+        #endregion
+    }
+
+    #region // |===== WhatIsItMainPage ==========================================================================================|
+
+    public partial class WhatIsItMainPage : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            PopulateImages();
+            InitializePage();
+        }
+
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string filename = string.Empty;
+                string ext = string.Empty;
+                filename = FileUpload1.FileName;
+                ext = Path.GetExtension(filename);
+
+                if (!string.IsNullOrEmpty(txtImageName.Text))
+                {
+                    filename = txtImageName.Text + ext;
+                }
+
+                //upload file to server
+                FileUpload1.PostedFile.SaveAs(Server.MapPath("~/UploadedImages/" + filename));
+            }
+            Response.Redirect("./WhatIsItMainPage.aspx");
+        }
+
+        private void PopulateImages()
+        {
+            foreach (string strFileName in Directory.GetFiles(Server.MapPath("~/UploadedImages/")))
+            {
+                var imageButton = new ImageButton();
+                var fileInfo = new FileInfo(strFileName);
+                imageButton.ImageUrl = "~/UploadedImages/" + fileInfo.Name;
+                imageButton.Width = Unit.Pixel(200);
+                imageButton.Height = Unit.Pixel(200);
+                imageButton.Style.Add("padding", "5px");
+                imageButton.Click += imageButton_Click;
+                Panel1.Controls.Add(imageButton);
+            }
+        }
+
+        private void imageButton_Click(object sender, ImageClickEventArgs e)
+        {
+            var wordToPronounce = (Path.GetFileNameWithoutExtension(((ImageButton) sender).ImageUrl.ToUpper()));
+
+            //show the image of the file just selected
+            FocusedImage.ImageUrl = ((ImageButton) sender).ImageUrl;
+            FocusedImageDesc.Text = string.Format("{0}<BR>", wordToPronounce);
+
+            //get the sound file to play
+            var soundFileToPlay = WordSoundService.GetSoundFileToPlay(wordToPronounce);
+
+            if (soundFileToPlay == String.Empty)
+            {
+                //indicate that the sound could not be found for this word
+                FocusedSound.Text =
+                    string.Format(
+                        "Sound file for the word {0} could not be found!",
+                        wordToPronounce);
+            }
+            else
+            {
+                //construct audio html tag with file to autoplay
+                FocusedSound.Text =
+                    string.Format(
+                        "<audio controls='controls' autoPlay='autoPlay'><source src='{0}' type='audio/mp3'/></audio>",
+                        soundFileToPlay);    
+            }
+        }
+
+        protected void btnShowHideNewImage_Click(object sender, EventArgs e)
+        {
+            if (UploadPanel.Visible)
+            {
+                // UploadPanel is currently visible
+                InitializePage();
+            }
+            else // UploadPanel is currently not visible
+            {
+                btnShowHideNewImage.Visible = false;
+                UploadPanel.Visible = true;
+            }
+        }
+
+        public void InitializePage()
+        {
+            btnShowHideNewImage.Visible = true;
+            btnShowHideNewImage.Text = "Upload a New Image ";
+            UploadPanel.Visible = false;
+        }
+
+        protected void CancelUpload_Click(object sender, EventArgs e)
+        {
+        }
+    }
+
+    #endregion ///////////////////////////////////////////////////////////////////////////////////
+}
