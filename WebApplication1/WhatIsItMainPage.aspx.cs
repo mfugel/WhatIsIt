61d8ed750e524fd55a0bf1a86fbcd79f4db2abdb
mfugel <markfugel@tnwinc.com>
2014-03-25 06:42:44 -0400

    Changed the way Forvo API is called. Cleanup part 1.

--- WebApplication1/WhatIsItMainPage.aspx.cs
+++ WebApplication1/WhatIsItMainPage.aspx.cs
@@ -1,175 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Net;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using Microsoft.Ajax.Utilities;
-using RestSharp;
 
 
 namespace WhatIsIt
 {
-
-    #region // |===== WordSoundService ==========================================================================================|
-
-    public class WordSoundService
-    {
-        public class Attributes
-        {
-            public int Total { get; set; }
-        }
-
-        public class Item
-        {
-            public int Id { get; set; }
-            public string Word { get; set; }
-            public string Addtime { get; set; }
-            public int Hits { get; set; }
-            public string Username { get; set; }
-            public string Sex { get; set; }
-            public string Country { get; set; }
-            public string Code { get; set; }
-            public string Langname { get; set; }
-            public string Pathmp3 { get; set; }
-            public string Pathogg { get; set; }
-            public object Rate { get; set; }
-            public object NumVotes { get; set; }
-            public int NumPositiveVotes { get; set; }
-        }
-
-        public class SoundResult
-        {
-            public Attributes Attributes { get; set; }
-            public List<Item> Items { get; set; }
-        }
-
-        public class SoundRequest
-        {
-            public string SiteUrl { get; set;}
-            public string ApiKey { get; set; }
-            public string Format { get; set; }
-            public string Action { get; set; }
-            
-            public string Language { get; set; }
-            public string Sex { get; set; }
-            public string Order { get; set; }
-            public string Limit { get; set; }
-            public string Type { get; set; }
-        }
-        
-
-        //Constants
-        public const string ApiKey = "114b9ec1f5cb114497cb5b00514bbdc6";
-
-        private const string BaseUrl =
-           "http://apifree.forvo.com" +
-           "/key/" + 
-           ApiKey +
-           "/format/json/action/word-pronunciations/language/en/sex/f/order/rate-desc/limit/1/word/";
-//         "/format/json/action/word-pronunciations/sex/f/order/rate-desc/limit/1/word/";
-        public const string AudioFileType = ".mp3";
-        public const string StorageFolderForSounds = @"UploadedSounds\";
-
-        public static string WordToSound = String.Empty;
-        public static string UrlToGetSoundFile = String.Empty;
-
-        public static string GetSoundFileToPlay(string word)
-        {
-            if (word == null) throw new ArgumentNullException("word");
-            var fullPathSoundStorageLocationForApp = System.AppDomain.CurrentDomain.BaseDirectory +
-                                                     StorageFolderForSounds;
-            
-            // Check if sound file is already there, if so...just return it
-            if (File.Exists(fullPathSoundStorageLocationForApp + word + AudioFileType))
-                return StorageFolderForSounds + word + AudioFileType;
-
-            // Sound file for this word is not there. We need to get it
-            WordToSound = word;
-
-            // Construct the request parameter settings
-            var soundRequest = new SoundRequest { 
-                                                  SiteUrl = "http://apifree.forvo.com",
-                                                  ApiKey = "114b9ec1f5cb114497cb5b00514bbdc6",
-                                                  Format = "json",
-                                                  Action = "word-pronunciations",
-                                                  Language = "en", 
-                                                  Sex = "f", 
-                                                  Limit = "1", 
-                                                  Order = "rate-desc", 
-                                                  Type = "word"
-                                                };
-            
-            UrlToGetSoundFile = BaseUrl + WordToSound;
-            var soundFileUri = GetSoundFromForvo();
-            
-            if (soundFileUri == String.Empty) return String.Empty;
-            
-            var soundFileName = DownloadSoundFile(soundFileUri);
-            
-            if (soundFileName == String.Empty) return String.Empty;
-            
-            return StorageFolderForSounds + soundFileName;
-        }
-
-        public static string GetSoundFromForvo()
-        {
-            var request = new RestRequest();
-            var soundresult = Execute<SoundResult>(request);
-            if (soundresult != null)
-            {
-                if (soundresult.Items.Count > 0)
-                {
-                    var pathmp3 = soundresult.Items[0].Pathmp3;
-                    if (!pathmp3.IsNullOrWhiteSpace())
-                    {
-                        return pathmp3;
-                    }
-                }
-            }
-            return String.Empty;
-        }
-
-        public static T Execute<T>(RestRequest request) where T : new()
-        {
-            //**** TBD: turn this into client.add parameters instead of hardcoded BaseUrl****
-            //IRestClient client = new RestClient("http://apifree.com/");
-            //client.AddDefaultHeader("key", ApiKey);
-            //client.AddDefaultUrlSegment("key", ApiKey);
-            //client.AddDefaultUrlSegment("language", "en");
-            //client.AddDefaultUrlSegment("format", "json");
-            //client.AddDefaultUrlSegment("action", "word-pronunciations");
-            //client.AddDefaultUrlSegment("limit", "1");
-            //client.AddDefaultUrlSegment("type", "word");
-
-            var client = new RestClient {BaseUrl = UrlToGetSoundFile};
-            var response = client.Execute<T>(request);
-            if (response.ErrorException != null)
-            {
-                const string message = "Error retrieving response from Forvo api. Check inner details for more info.";
-                var forvoException = new ApplicationException(message, response.ErrorException);
-                throw forvoException;
-            }
-            return response.Data;
-        }
-
-        public static string DownloadSoundFile(string soundFileUri)
-        {
-            var u = new Uri(soundFileUri);
-            var fileName = WordToSound + AudioFileType;
-            var pathWithSoundFileName = System.AppDomain.CurrentDomain.BaseDirectory + "UploadedSounds\\" + fileName;
-            using (var client = new WebClient())
-            {
-                client.DownloadFile(soundFileUri, pathWithSoundFileName);
-            }
-            return fileName;
-        }
-
-        #endregion
-    }
-
-    #region // |===== WhatIsItMainPage ==========================================================================================|
-
     public partial class WhatIsItMainPage : Page
     {
         protected void Page_Load(object sender, EventArgs e)
@@ -182,20 +18,28 @@ namespace WhatIsIt
         {
             if (FileUpload1.HasFile)
             {
-                string filename = string.Empty;
-                string ext = string.Empty;
-                filename = FileUpload1.FileName;
-                ext = Path.GetExtension(filename);
-
-                if (!string.IsNullOrEmpty(txtImageName.Text))
-                {
-                    filename = txtImageName.Text + ext;
-                }
-
                 //upload file to server
-                FileUpload1.PostedFile.SaveAs(Server.MapPath("~/UploadedImages/" + filename));
+                string determineFilePath = DetermineFilePath(FileUpload1.FileName, txtImageName.Text);
+                FileUpload1.PostedFile.SaveAs(determineFilePath);
+
+                Response.Redirect("./WhatIsItMainPage.aspx");
             }
-            Response.Redirect("./WhatIsItMainPage.aspx");
+        }
+
+        private string DetermineFilePath(string sourceFilename, string desiredFileName = null) 
+        {
+            var resultingFilename = "";
+            var ext = Path.GetExtension(sourceFilename);
+
+            if (!string.IsNullOrEmpty(desiredFileName))
+            {
+                resultingFilename = desiredFileName + ext;
+            }
+            else
+            {
+                resultingFilename = sourceFilename;
+            }
+            return Server.MapPath("~/UploadedImages/" + resultingFilename);
         }
 
         private void PopulateImages()
@@ -238,7 +82,7 @@ namespace WhatIsIt
                 FocusedSound.Text =
                     string.Format(
                         "<audio controls='controls' autoPlay='autoPlay'><source src='{0}' type='audio/mp3'/></audio>",
-                        soundFileToPlay);    
+                        soundFileToPlay);
             }
         }
 
@@ -267,6 +111,4 @@ namespace WhatIsIt
         {
         }
     }
-
-    #endregion ///////////////////////////////////////////////////////////////////////////////////
 }
---
+++ WebApplication1/WordSoundService.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Web;
+using System.Web.Services.Description;
+using Microsoft.Ajax.Utilities;
+using RestSharp;
+
+
+namespace WhatIsIt
+{
+    public class WordSoundService
+    {
+        public class Attributes
+        {
+            public int Total { get; set; }
+        }
+
+        public class Item
+        {
+            public int Id { get; set; }
+            public string Word { get; set; }
+            public string Addtime { get; set; }
+            public int Hits { get; set; }
+            public string Username { get; set; }
+            public string Sex { get; set; }
+            public string Country { get; set; }
+            public string Code { get; set; }
+            public string Langname { get; set; }
+            public string Pathmp3 { get; set; }
+            public string Pathogg { get; set; }
+            public object Rate { get; set; }
+            public object NumVotes { get; set; }
+            public int NumPositiveVotes { get; set; }
+        }
+
+        public class SoundResult
+        {
+            public Attributes Attributes { get; set; }
+            public List<Item> Items { get; set; }
+        }
+
+        public class SoundRequest
+        {
+            public string SiteUrlName = "siteUrl";
+            public string SiteUrlValue = "http://apifree.forvo.com/";
+            public string ApiKeyName = "key";
+            public string ApiKeyValue = "114b9ec1f5cb114497cb5b00514bbdc6";
+            public string FormatName = "format";
+            public string FormatValue = "json";
+            public string ActionName = "action";
+            public string ActionValue = "word-pronunciations";
+            public string LanguageName = "language";
+            public string LanguageValue { get; set; }
+            public string SexName = "sex";
+            public string SexValue { get; set; }
+            public string OrderName = "order";
+            public string OrderValue = "rate-desc";
+            public string LimitName = "limit";
+            public string LimitValue = "1";
+            public string TypeName = "type";
+            public string TypeValue { get; set; }
+            public string WordName = "word";
+            public string WordValue { get; set; }
+
+        }
+        
+
+        //Constants
+        public const string AudioFileType = ".mp3";
+        public const string StorageFolderForSounds = @"UploadedSounds\";
+
+        public static string WordToSound = String.Empty;
+        public static string UrlToGetSoundFile = String.Empty;
+
+        public static string GetSoundFileToPlay(string word)
+        {
+            if (word == null) throw new ArgumentNullException("word");
+            var fullPathSoundStorageLocationForApp = System.AppDomain.CurrentDomain.BaseDirectory + StorageFolderForSounds;
+
+            // Check if sound file is already there, if so...just return it
+            string existingFileLocation;
+            if (CheckIfSoundfileAlreadyExists(word, fullPathSoundStorageLocationForApp, out existingFileLocation)) return existingFileLocation;
+
+            // Sound file for this word is not there. We need to get it
+            WordToSound = word;
+
+            // Construct the request parameter settings
+            var soundRequest = new SoundRequest
+            {
+                ApiKeyName = "key",
+                ActionName = "action",
+                ActionValue = "word-pronunciations",
+                LanguageName = "language",
+                LanguageValue = "en",
+                SexName = "sex",
+                SexValue = "m",
+                OrderName = "order",
+                OrderValue = "rate-desc",
+                LimitName = "limit",
+                LimitValue = "1",
+                TypeName = "type",
+                TypeValue = "word",
+                WordValue = word
+            };
+
+            var soundFileUri = GetSoundFromForvo(soundRequest);
+            
+            if (soundFileUri == String.Empty) return String.Empty;
+            
+            var soundFileName = DownloadSoundFile(soundFileUri);
+            
+            if (soundFileName == String.Empty) return String.Empty;
+            
+            return StorageFolderForSounds + soundFileName;
+        }
+
+        private static bool CheckIfSoundfileAlreadyExists(string word, string fullPathSoundStorageLocationForApp, out string existingFileLocation)
+        {
+            existingFileLocation = null;
+            if (!File.Exists(fullPathSoundStorageLocationForApp + word + AudioFileType)) return false;
+            existingFileLocation = StorageFolderForSounds + word + AudioFileType;
+            return true;
+        }
+
+        public static string GetSoundFromForvo(SoundRequest soundRequest)
+        {
+            var request = new RestRequest("/" + soundRequest.ApiKeyName   + "/{" + soundRequest.ApiKeyName   + "}" +
+                                          "/" + soundRequest.ActionName   + "/{" + soundRequest.ActionName   + "}" +
+                                          "/" + soundRequest.LanguageName + "/{" + soundRequest.LanguageName + "}" +
+                                          "/" + soundRequest.FormatName   + "/{" + soundRequest.FormatName   + "}" +
+                                          "/" + soundRequest.SexName      + "/{" + soundRequest.SexName      + "}" +
+                                          "/" + soundRequest.OrderName    + "/{" + soundRequest.OrderName    + "}" +
+                                          "/" + soundRequest.LimitName    + "/{" + soundRequest.LimitName    + "}" +
+                                          "/" + soundRequest.TypeName     + "/{" + soundRequest.TypeName     + "}" +
+                                          "/" + soundRequest.WordName     + "/{" + soundRequest.WordName     + "}");
+                                         // "/language/{language}/format/{format}/sex/{sex}/order/{order}/limit/{limit}/type/{type}/word/{word}");
+            var soundresult = Execute<SoundResult>(request, soundRequest);
+            if (soundresult != null)
+            {
+                if (soundresult.Items.Count > 0)
+                {
+                    var pathmp3 = soundresult.Items[0].Pathmp3;
+                    if (!pathmp3.IsNullOrWhiteSpace())
+                    {
+                        return pathmp3;
+                    }
+                }
+            }
+            return String.Empty;
+        }
+
+        public static T Execute<T>(RestRequest request, SoundRequest soundRequest) where T : new()
+        {
+            IRestClient client = new RestClient(soundRequest.SiteUrlValue);
+            request.AddParameter(soundRequest.ApiKeyName, soundRequest.ApiKeyValue, ParameterType.UrlSegment);
+            request.AddParameter(soundRequest.ActionName, soundRequest.ActionValue, ParameterType.UrlSegment);
+            request.AddParameter(soundRequest.LanguageName, soundRequest.LanguageValue, ParameterType.UrlSegment);
+            request.AddParameter(soundRequest.FormatName, soundRequest.FormatValue, ParameterType.UrlSegment);
+            request.AddParameter(soundRequest.SexName, soundRequest.SexValue, ParameterType.UrlSegment);
+            request.AddParameter(soundRequest.OrderName, soundRequest.OrderValue, ParameterType.UrlSegment);
+            request.AddParameter(soundRequest.LimitName, soundRequest.LimitValue, ParameterType.UrlSegment);
+            request.AddParameter(soundRequest.TypeName, soundRequest.TypeValue, ParameterType.UrlSegment);
+            request.AddParameter(soundRequest.WordName, soundRequest.WordValue, ParameterType.UrlSegment);
+
+            var response = client.Execute<T>(request);
+            if (response.ErrorException != null)
+            {
+                const string message = "Error retrieving response from Forvo api. Check inner details for more info.";
+                var forvoException = new ApplicationException(message, response.ErrorException);
+                throw forvoException;
+            }
+            return response.Data;
+        }
+
+        public static string DownloadSoundFile(string soundFileUri)
+        {
+            var u = new Uri(soundFileUri);
+            var fileName = WordToSound + AudioFileType;
+            var pathWithSoundFileName = System.AppDomain.CurrentDomain.BaseDirectory + "UploadedSounds\\" + fileName;
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(soundFileUri, pathWithSoundFileName);
+            }
+            return fileName;
+        }
+    }
+}
